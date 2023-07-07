1c823dcb1aa5d2d4942fe16d33d886dd45507b93
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-07-07 18:53:03 -0300

    :sparkles: feat: prefix class members with 'm_', turn class variables private and write them getters, also add name and version to the constructor parameters

--- src/cli.cpp
+++ src/cli.cpp
@@ -13,23 +13,51 @@ bool is_flag(std::string& argument) {
 /* ----- PUBLIC ----- */
 
 // Constructor
-Cli::Cli(int& argument_count, char* argument_vector[]) {
-  this -> argument_count;
-  parse_arguments(argument_count, argument_vector);
-  parse_flags(arguments);
+Cli::Cli(
+  int& arguments_count,
+  char* arguments_vector[],
+  std::string name,
+  std::string version
+) {
+  m_arguments_count = arguments_count;
+  m_name = name;
+  m_version = version;
+
+  parse_arguments(arguments_count, arguments_vector);
+  parse_flags(m_arguments);
+}
+
+int Cli::get_arguments_count() {
+  return m_arguments_count;
+}
+
+std::vector<std::string> Cli::get_arguments() {
+  return m_arguments;
+}
+
+std::vector<std::string> Cli::get_flags() {
+  return m_flags;
+}
+
+std::string Cli::get_name() {
+  return m_name;
+}
+
+std::string Cli::get_version() {
+  return m_version;
 }
 
 /* ----- PRIVATE ----- */
-void Cli::parse_arguments(int& argument_count, char* argument_vector[]){
-  for (int i = 0; i < argument_count; i++) {
-    arguments.push_back(argument_vector[i]);
+void Cli::parse_arguments(int& arguments_count, char* arguments_vector[]){
+  for (int i = 0; i < arguments_count; i++) {
+    m_arguments.push_back(arguments_vector[i]);
   }
 }
 
 void Cli::parse_flags(std::vector<std::string>& arguments) {
   for (std::string argument : arguments) {
     if (is_flag(argument)) {
-      flags.push_back(argument);
+      m_flags.push_back(argument);
 
       // If needed to remove '-', use: argument.substr(1, argument.size());
     }
--- src/cli.h
+++ src/cli.h
@@ -9,17 +9,32 @@ bool is_flag(std::string& argument);
 
 class Cli {
 public:
-  int argument_count;
-  std::vector<std::string> arguments;
-  std::vector<std::string> flags;
+  Cli(
+    int& arguments_count,
+    char* arguments_vector[],
+    std::string name,
+    std::string version
+  );
 
-  Cli(int& argument_count, char* argument_vector[]);
+  int get_arguments_count();
+  std::vector<std::string> get_arguments();
+  std::vector<std::string> get_flags();
+  
+  std::string get_name();
+  std::string get_version();
 
-private: 
+private:
+  int m_arguments_count;
+  std::vector<std::string> m_arguments;
+  std::vector<std::string> m_flags;
+
+  std::string m_name;
+  std::string m_version;
+
   /// @brief A method that takes an argument list and parse it.
   /// @param argument_count - The number of arguments. Normally referred as argc.
   /// @param argument_vector - The list of arguments. Usually referred as argv.
-  void parse_arguments(int& argument_count, char* argument_vector[]);
+  void parse_arguments(int& arguments_count, char* arguments_vector[]);
 
   /// @brief A method that takes an argument vector, parse it and store the flags.
   /// @param arguments - A vector with arguments.
