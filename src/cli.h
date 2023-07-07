df3ab27007fcf9adaac2a1bec63cc8dc166f1d9d
Kauê Fraga Rodrigues <kauefragarodrigues456@gmail.com>
2023-07-07 15:05:08 -0300

    :recycle: refactor: write doxygen comments and improve the flag's loop

--- src/cli.h
+++ src/cli.h
@@ -2,8 +2,9 @@
 #include <vector>
 #include <string>
 
-// using arguments_t = std::vector<std::string> 
-
+/// @brief Verify if an argument is a flag by searching for '-' at the initial position (0) of the argument.
+/// @param argument - The argument to be verified.
+/// @returns A boolean that is true when the given argument matches the condition.
 bool is_flag(std::string& argument) {
   if (argument.find('-') == 0) {
     return true;
@@ -12,36 +13,35 @@ bool is_flag(std::string& argument) {
   return false;
 }
 
-// Pay attention to optimizations like pass by reference
 class Cli {
 public:
   int argument_count;
   std::vector<std::string> arguments;
   std::vector<std::string> flags;
-  Cli(int argument_count, char* argument_vector[]) {
+
+  Cli(int& argument_count, char* argument_vector[]) {
     this -> argument_count;
     parse_arguments(argument_count, argument_vector);
     parse_flags(arguments);
   }
 
-  /// @brief A method that takes an argument list and parse it. 
+  /// @brief A method that takes an argument list and parse it.
   /// @param argument_count - The number of arguments. Normally referred as argc.
   /// @param argument_vector - The list of arguments. Usually referred as argv.
-  /// @returns A list of the arguments that exists.
-  void parse_arguments(int argument_count, char* argument_vector[]) {
-    // Loop through the argument vector pushing back all the values to a string vector
+  void parse_arguments(int& argument_count, char* argument_vector[]) {
     for (int i = 0; i < argument_count; i++) {
       arguments.push_back(argument_vector[i]);
     }
   }
 
+  /// @brief A method that takes an argument vector, parse it and store the flags.
+  /// @param arguments - A vector with arguments.
   void parse_flags(std::vector<std::string>& arguments) {
-    for (int i = 0; i < arguments.size(); i++) {
-      if (is_flag(arguments[i])) {
-        flags.push_back(arguments[i]);
+    for (std::string argument : arguments) {
+      if (is_flag(argument)) {
+        flags.push_back(argument);
 
-        // If needed to remove '-', use
-        // flags.push_back(argument.substr(1, argument.size()));
+        // If needed to remove '-', use: argument.substr(1, argument.size());
       }
 
       continue;
